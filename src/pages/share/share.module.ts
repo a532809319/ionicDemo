c368cb293a66d587f532b4a90f14fc048d1cb6e4
yamss <lvchaoyi.1986@163.com>
2018-06-04 11:22:28 +0800

    格式化代码插件使用调试

--- src/pages/share/share.module.ts
+++ src/pages/share/share.module.ts
@@ -1,13 +1,9 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { SharePage } from './share';
+import { NgModule } from "@angular/core";
+import { IonicPageModule } from "ionic-angular";
+import { SharePage } from "./share";
 
 @NgModule({
-  declarations: [
-    SharePage,
-  ],
-  imports: [
-    IonicPageModule.forChild(SharePage),
-  ],
+  declarations: [SharePage],
+  imports: [IonicPageModule.forChild(SharePage)]
 })
 export class SharePageModule {}
--- src/pages/share/share.ts
+++ src/pages/share/share.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
 import {
-	IonicPage,
-	NavController,
-	NavParams,
-	ActionSheetController
+  IonicPage,
+  NavController,
+  NavParams,
+  ActionSheetController
 } from "ionic-angular";
 
 declare var Wechat;
@@ -16,73 +16,70 @@ declare var Wechat;
 
 @IonicPage()
 @Component({
-	selector: "page-share",
-	templateUrl: "share.html"
+  selector: "page-share",
+  templateUrl: "share.html"
 })
 export class SharePage {
-	constructor(
-		public navCtrl: NavController,
-		public navParams: NavParams,
-		public actionSheet: ActionSheetController
-	) { }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public actionSheet: ActionSheetController
+  ) {}
 
-	ionViewDidLoad() {
-		console.log("ionViewDidLoad SharePage");
-	}
-	open() {
-		this.actionSheet
-			.create({
-				buttons: [
-					{
-						text: "微信",
-						handler: () => {
-							Wechat.isInstalled(
-								function (installed) {
-									if (installed) {
-										Wechat.share(
-											{
-												message: {
-													title: "测试微信分享",
-													description:
-														"这里是分享内容描述",
-													thumb:
-														"www/assets/imgs/shouye01.png",
-													media: {
-														type: Wechat.Type.IMAGE,
-														image:
-															"www/assets/imgs/shouye01.png"
-													}
-												},
-												scene: Wechat.Scene.SESSION
-											},
-											success => {
-												alert("成功");
-											},
-											error => {
-												debugger;
-												alert("失败1" + error);
-											}
-										);
-									} else {
-										alert("Please install Wechat");
-									}
-								},
-								function (reason) {
-									alert("Failed: " + reason);
-								}
-							);
-						}
-					},
-					{
-						text: "朋友圈",
-						handler: () => { }
-					},
-					{
-						text: "取消",
-						role: "cancle"
-					}
-				]
-			})
-			.present();
-	}
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad SharePage");
+  }
+  open() {
+    this.actionSheet
+      .create({
+        buttons: [
+          {
+            text: "微信",
+            handler: () => {
+              Wechat.isInstalled(
+                function(installed) {
+                  if (installed) {
+                    Wechat.share(
+                      {
+                        message: {
+                          title: "测试微信分享",
+                          description: "这里是分享内容描述",
+                          thumb: "www/assets/imgs/shouye01.png",
+                          media: {
+                            type: Wechat.Type.IMAGE,
+                            image: "www/assets/imgs/shouye01.png"
+                          }
+                        },
+                        scene: Wechat.Scene.SESSION
+                      },
+                      success => {
+                        alert("成功");
+                      },
+                      error => {
+                        debugger;
+                        alert("失败1" + error);
+                      }
+                    );
+                  } else {
+                    alert("Please install Wechat");
+                  }
+                },
+                function(reason) {
+                  alert("Failed: " + reason);
+                }
+              );
+            }
+          },
+          {
+            text: "朋友圈",
+            handler: () => {}
+          },
+          {
+            text: "取消",
+            role: "cancle"
+          }
+        ]
+      })
+      .present();
+  }
 }

40a904e17ee9983fb5c8bd7e07316ae1de22aaf7
yamss <lvchaoyi.1986@163.com>
2018-08-16 10:19:51 +0800

    为swiper添加项增加判断条件，不能无条件添加

--- src/pages/calendar/calendar.module.ts
+++ src/pages/calendar/calendar.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { CalendarPage } from "./calendar";
-import { CalendarComponent } from "../../components/calendar/calendar";
 import { CalendarModule } from "primeng/calendar";
 import { ButtonModule } from "primeng/button";
 
 @NgModule({
-  declarations: [CalendarPage, CalendarComponent],
+  declarations: [CalendarPage],
   imports: [
     CalendarModule,
     ButtonModule,
